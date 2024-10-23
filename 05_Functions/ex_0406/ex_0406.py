dd307b4b19c58b9648f390a510a08222a6a14afe
Abel Chávez Frutos <achf16@gmail.com>
2024-10-23 10:03:12 -0400

    ex_0406

---
+++ 05_Functions/ex_0406/ex_0406.py
@@ -0,0 +1,22 @@
+# Rewrite the exercise 3.2, this time using a function that takes two parameter(hours, rate)
+# To have a returned value to the main functions is a requirement of the exercise, but it's not necessary
+
+def computepay(hours, rate):
+    if worked_hours <= 40:
+        return worked_hours * rate_per_hours
+    else:
+        return worked_hours * rate_per_hours + (worked_hours - 40) * rate_per_hours * 0.5
+
+try:
+    worked_hours = float(input('Enter Hours: '))
+except:
+    print('Error, please enter a numeric input')
+    quit()
+
+try:
+    rate_per_hours = float(input('Enter Rate: '))
+except:
+    print('Error, please enter a numeric input')
+    quit()
+
+print('Pay: ',computepay(worked_hours, rate_per_hours))
