2a64336327fc7d7fe5510d1c10c006b3ed15022f
Abel Chávez Frutos <achf16@gmail.com>
2024-10-27 12:18:48 -0400

    ex_0702

--- 07_Files/ex_0702/ex_0702.py
+++ 07_Files/ex_0702/ex_0702.py
@@ -2,7 +2,6 @@
 #Global definitions
 #------------------------
 counter = 0
-
 #------------------------
 # Fx Definitions
 #------------------------
@@ -12,27 +11,39 @@ def asking_for_a_file_name():
         fhand = open(fname)
     except:
         print(f"Invalid file. {fname} does not exist.")
-        asking_for_a_file_name()
-    searching_line(fhand)
+        return asking_for_a_file_name()
+    return fhand
 
 def searching_line(file_handle):
+    total = 0
+    c = 0
     for line in file_handle:
-        line = line.rstrip()
-        if line.startswith("X-DSPAM-Confidence:"):
-            lines_counter()
-            #line.find(':') + 1
-            # cases: there are a blanc space before the number, there are a letter before the number
-            #       there aren't blanc after the number, there a space after
-            # There is a way, slicing testing each index after the strig until get a traceback, and the index to
-            # convert to float will be ErrorIndex-1 at the end
+        line = line.strip()
+        s = "X-DSPAM-Confidence:"
+        if line.startswith(s):
+            c = lines_counter()
+            index_bfloat = len(s)
+            total += searching_float(line,index_bfloat)
+    return total , c
 
 def lines_counter():
     global counter
     counter += 1
     return counter
 
+def searching_float(l, index_bf):
+    while l[index_bf] == ' ': index_bf += 1
+    index_ff = index_bf + 1
+    if l[index_bf] == '0': index_ff += 2
+    try:
+        while float(l[index_bf: index_ff]) and l(index_ff) != ' ' and index_ff < len(l): index_ff += 1
+    except: pass
+    return float(l[index_bf : ])
+
+def ave(total_plus_counter):
+    return total_plus_counter[0] / total_plus_counter[1]
 
 #------------------------
 # MAIN
 #------------------------
-asking_for_a_file_name()
+print('Average spam confidence:', ave(searching_line(asking_for_a_file_name())))
