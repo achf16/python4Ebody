1eff1b0d8fbf65109da708c0a84cf7a04cc73aa7
Abel Chávez Frutos <achf16@gmail.com>
2024-11-14 10:15:05 -0400

    ex_0804

--- 08_Lists/ex_0804/ex_0804.py
+++ 08_Lists/ex_0804/ex_0804.py
@@ -1,6 +1,8 @@
 #-----------------------------------
 #   Libraries and Global definitions
 #-----------------------------------
+import re
+uniq_list = list()
 
 #-----------------------------------
 #   Function definitions
@@ -19,11 +21,22 @@ def init():
 def inter_line_traverser(fh):
     for line in fh:
         words = line.split()
-        if len(words) == 0: continue
-        # Here should stay the line traverser with an other for
+        if len(words) == 0: continue    # empty line
+        line_traverser(words)
+
+def line_traverser(w):
+    global uniq_list
+    for word in w:
+        word = re.sub(r'\W', '', word)
+        print(word)
+        if word.lower() in uniq_list: continue
+        uniq_list.append(word)
 
 #-----------------------------------
 #   Main
 #-----------------------------------
 while True:
+    # TODO: the list should be sorted and printed
     inter_line_traverser(init())
+    uniq_list.sort()
+    print(uniq_list)
