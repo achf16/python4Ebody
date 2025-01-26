25ab09ae79d1143a6ec354cdfe912a0bd331a83c
Abel Chávez Frutos <achf16@gmail.com>
2025-01-26 18:02:53 -0400

    feat: Add reception mode selection and descriptions
    This commit introduces a reception mode selection for handling incoming data.
    It provides a description of the four possible ways to solve the reception problem,
    allowing the user to select the desired approach, it breaks the code to do a specific reception function for every case.
    No breaking changes.

--- 12_Networked_programs/ex_1205/ex_1205.py
+++ 12_Networked_programs/ex_1205/ex_1205.py
@@ -33,26 +33,47 @@ def server_separator(url):
 
 def recv_process(mysock):
     #TODO: OS libraries for clean temp file
+    print("Select the reception mode or 'done' to finish: \n"
+            "There are four ways to solve this problem:\n"
+            "   1- The first one, consist on:\n"
+            "       *   receive the whole doc,\n"
+            "       *   store on the secondary memory,\n"
+            "       *   to find the \\n\\n pattern, because this is the end of the header,\n"
+            "       *   and finally, seek that index and print all after that\n"
+            "   2- The other way consist on:\n"
+            "       *   receive chunks on the primary memory,\n"
+            "       *   traverse the received data until found the first \\n\\n pattern\n"
+            "       *   and print from there to the end\n"
+            "   3- The third way is a mix of the two above:\n"
+            "       *   consist on traverse any received data chunk until found \\n\\n pattern(primary memory)\n"
+            "       *   then store all from there to the end on the secondary memory\n"
+            "       *   and print\n"
+            "   4- The fourth way is a mix of the first two to:\n"
+            "       *   consist on save the whole document on the secondary memory,\n"
+            "       *   traverse until the pattern \\n\\n\n"
+            "       *   write from that index until the end in other temp file\n"
+            "       *   and finally print")
+    while True:
+        mode = input("Write here: ").lower().strip()
+        match mode:
+            case '1':
+                mode1()
+                break
+            case '2':
+                mode2()
+                break
+            case '3':
+                mode3()
+                break
+            case '4':
+                mode4()
+                break
+            case 'done':
+                quit()
+            case _:
+                print("Invalid mode")
 
-    #There are four ways of solve this problem:
-    #   - The first one, consist on:
-    #       *   receive the whole doc,
-    #       *   store on the secondary memory,
-    #       *   to find the \n\n pattern, because this is the end of the header,
-    #       *   and finally, seek that index and print all after that
-    #   - The other way consist on:
-    #       *   receive chunks on the primary memory,
-    #       *   traverse the received data until found the first \n\n pattern
-    #       *   and print from there to the end
-    #   - The third way is a mix of the two above:
-    #       *   consist on traverse any received data chunk until found \n\n pattern(primary memory)
-    #       *   then store all from there to the end on the secondary memory
-    #       *   and print
-    #   - The fourth way is a mix of the first two to:
-    #       *   consist on save the whole document on the secondary memory,
-    #       *   traverse until the pattern \n\n
-    #       *   write from that index until the end in other temp file
-    #       *   and finally print
+    quit()
 
     
 
